d6f9bbe1c4464981f89ab9417fbfa33b4b42be7f
victus-est-deus <n_oralbai@kbtu.kz>
2023-02-28 15:32:38 +0600

    fixing categories

--- lab5/src/app/top-bar/top-bar.component.ts
+++ lab5/src/app/top-bar/top-bar.component.ts
@@ -9,17 +9,7 @@ import { Product, products } from '../products';
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent {
-  ngOnInit(): void {// First get the product category from the current route.
-    const routeParams = this.route.snapshot.paramMap;
-    const productCategoriesFromRoute = String(routeParams.get('CategoryCategories'));
   
-    // Find the product that correspond with the categories provided in route.
-    this.category = categories.find(categories => categories.category === productCategoriesFromRoute);
-    
-  }
-  category: categories | undefined;
-  constructor(
-    private route: ActivatedRoute,
-    
-  ) { }
+    categories = categories;
+  
 }
