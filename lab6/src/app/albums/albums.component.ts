3487c44c392ab2cafe71b777bed0f38e3facdee7
Nurzhan <123543718+victus-est-deus@users.noreply.github.com>
2023-03-08 20:16:35 +0600

    Albums component added

--- lab6/src/app/albums/albums.component.ts
+++ lab6/src/app/albums/albums.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+import {Album} from "../models/album";
+import {AlbumsService} from "./albums.service";
+
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
   styleUrls: ['./albums.component.css']
 })
 export class AlbumsComponent {
+  albums: Album[];
 
+  constructor(private albumsService: AlbumsService) {
+    this.albums = [];
+  }
+
+  ngOnInit(): void {
+    this.albumsService.getAlbums().subscribe(
+      (response) => {
+        this.albums = response.slice(0, 15);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  deleteAlbum(album: Album): void {
+    const index = this.albums.indexOf(album);
+    if (index !== -1) {
+      this.albums.splice(index, 1);
+    }
+  }
 }
--- lab6/src/app/app.module.ts
+++ lab6/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AlbumsComponent } from './albums/albums.component';
-import { AboutComponent } from './about/about.component';
-import { AlbumsPhotoComponent } from './albums-photo/albums-photo.component';
-import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {AlbumsComponent} from './albums/albums.component';
+import {AboutComponent} from './about/about.component';
+import {AlbumsPhotoComponent} from './albums-photo/albums-photo.component';
+import {AlbumDetailComponent} from './album-detail/album-detail.component';
 
 @NgModule({
   declarations: [
@@ -20,9 +21,11 @@ import { AlbumDetailComponent } from './album-detail/album-detail.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
