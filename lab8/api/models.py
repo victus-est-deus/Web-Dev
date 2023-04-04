806db8a8885a3a27122aaeb8c4c25bcda1856cc0
victus-est-deus <n_oralbai@kbtu.kz>
2023-04-04 14:41:27 +0600

    adding models and api endpoint /products

---
+++ lab8/api/models.py
@@ -0,0 +1,35 @@
+
+from django.db import models
+
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    count = models.IntegerField()
+    is_active = models.BooleanField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count,
+            'is_active': self.is_active,
+            'category': self.category.name
+
+        }
---
+++ lab8/api/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    path('products/', views.product_list),
+    path('products/<int:product_id>', views.product),
+    path('category/', views.product_categories),
+    path('category/<int:category_id>', views.category_by),
+    path('category/<int:category_id>/products', views.products_by_category)
+
+]
