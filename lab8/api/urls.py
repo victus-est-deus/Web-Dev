d3895f14095aa073e0ad3256404e534d5acff1d6
victus-est-deus <n_oralbai@kbtu.kz>
2023-04-04 14:52:36 +0600

    adding the rest of api endpoints

--- lab8/api/urls.py
+++ lab8/api/urls.py
@@ -4,9 +4,9 @@ from api import views
 
 urlpatterns = [
     path('products/', views.product_list),
-    path('products/<int:product_id>', views.product),
+    path('products/<int:product_id>/', views.product),
     path('category/', views.product_categories),
-    path('category/<int:category_id>', views.category_by),
-    path('category/<int:category_id>/products', views.products_by_category)
+    path('category/<int:category_id>/', views.category_by),
+    path('category/<int:category_id>/products/', views.products_by_category)
 
 ]
