ab01e1307305cb9caa7454e53d37ee5b2f9d723e
victus-est-deus <n_oralbai@kbtu.kz>
2023-04-11 17:06:35 +0600

    adding views and apiendpoints

--- lab9/lab9/api/admin.py
+++ lab9/lab9/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from api.models import Company
+from api.models import Vacancy
 # Register your models here.
+
+admin.site.register(Company),
+admin.site.register(Vacancy),
+
